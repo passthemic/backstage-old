3f8fed6e7c9a2486274699d8b1f91acb0a39f9ba
James Stubblefield <jameswilliamiii@gmail.com>
2013-05-09 17:02:03 -0500

    update verse controller with actions

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,2 +1,21 @@
 class VersesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  respond_to :json
+
+
+  def index
+    @verses = Verse.all
+    respond_with({:verses => @verses}.as_json)
+  end
+
+  def show
+    @verse = Verse.find(params[:id])
+    respond_with(@verse)
+  end
+
+  def create
+    @verse = Verse.create(params[:verse])
+    respond_with(@verse)
+  end
+
 end
