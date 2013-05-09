9a2fda723f77966fbd800adf5fd2cc9c3f235db3
James Stubblefield <jameswilliamiii@gmail.com>
2013-05-09 16:25:12 -0500

    updated to add heroku_env file info

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#load heroku_env files for development
+heroku_env = File.join(Rails.root, 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 # Initialize the rails application
 PtmFileDemo::Application.initialize!
